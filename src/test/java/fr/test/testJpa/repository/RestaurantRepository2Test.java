4655821e27e3fb5cbfb943526252ab42f0805b64
trong.nguyen <trongthanh.nguyenmail@yahoo.fr>
2016-10-20 12:10:55 +0200

    rename for standards of testing junit

--- src/test/java/fr/test/testJpa/repository/RestaurantRepository2Test.java
+++ src/test/java/fr/test/testJpa/repository/RestaurantRepository2Test.java
@@ -1,36 +1,40 @@
 package fr.test.testJpa.repository;
 
 import fr.test.testJpa.model.Restaurant;
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import fr.test.testJpa.repository.RestaurantRepository;
+
+import org.junit.*;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration({"classpath*:testJpa-dao-context-oracle.xml"})
-@Rollback(value = false)
+@ContextConfiguration({"classpath*:testJpa-dao-test-context.xml"})
 @Transactional
-
-public class RestaurantRepositoryTestOracle {
+public class RestaurantRepository2Test {
 
 	@Autowired
 	private RestaurantRepository repo;
 
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+
 	@Test
-	public void testSaveOracle() {
+	public void testSave2() {
 		Restaurant restaurant = new Restaurant();
-		restaurant.setTitle("title1");
+		restaurant.setTitle("title2");
 
 		Restaurant restaurantSaved = repo.save(restaurant);
 		Assert.assertNotNull(restaurantSaved);
-		Assert.assertEquals("title1", restaurantSaved.getTitle());
-
+		Assert.assertEquals("title2", restaurantSaved.getTitle());
 	}
 
 }
--- src/test/java/fr/test/testJpa/repository/RestaurantRepositoryOracleTest.java
+++ src/test/java/fr/test/testJpa/repository/RestaurantRepositoryOracleTest.java
@@ -1,39 +1,34 @@
 package fr.test.testJpa.repository;
 
+import fr.test.testJpa.model.Restaurant;
 import org.junit.*;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
-import fr.test.testJpa.model.Restaurant;
-
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration({"classpath*:testJpa-dao-test-context.xml"})
+@ContextConfiguration({"classpath*:testJpa-dao-context-oracle.xml"})
 @Transactional
-public class RestaurantRepositoryTest {
+
+@Rollback(value = false)
+@Ignore
+public class RestaurantRepositoryOracleTest {
 
 	@Autowired
 	private RestaurantRepository repo;
 
-	@Before
-	public void setUp() throws Exception {
-	}
-
-	@After
-	public void tearDown() throws Exception {
-	}
-
-
 	@Test
-	public void testSave() {
+	public void testSaveOracle() {
 		Restaurant restaurant = new Restaurant();
 		restaurant.setTitle("title1");
 
 		Restaurant restaurantSaved = repo.save(restaurant);
 		Assert.assertNotNull(restaurantSaved);
 		Assert.assertEquals("title1", restaurantSaved.getTitle());
+
 	}
 
 }
