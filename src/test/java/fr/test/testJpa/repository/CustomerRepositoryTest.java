3728a2647aafe976312cd13d02b5be596d9989ab
trong.nguyen <trongthanh.nguyenmail@yahoo.fr>
2016-10-25 11:04:12 +0200

    test case ManyToMany

--- src/test/java/fr/test/testJpa/repository/CustomerRepositoryTest.java
+++ src/test/java/fr/test/testJpa/repository/CustomerRepositoryTest.java
@@ -8,14 +8,16 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration({"/testJpa-dao-test-context.xml"})
+@ContextConfiguration({"/testJpa-dao-context-oracle.xml"})
 @Transactional
 
+@Rollback(value = false)
 public class CustomerRepositoryTest {
 
 	@Autowired
